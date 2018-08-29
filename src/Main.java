1526228d01492986b2a86d11ab30246bc84720bd
Mobidic26869 <59160114@go.buu.ac.th>
2018-08-29 14:36:47 +0700

    Lab3_2 commit

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        //String input = JOptionPane.showInputDialog("ใส่ตัวเลข 1 ตัว","0");
-        //JOptionPane.showMessageDialog(null,"input","title",JOptionPane.INFORMATION_MESSAGE);
-        int ans = JOptionPane.showConfirmDialog(null,"รักหรือเปล่า","ถามเธอ",JOptionPane.YES_NO_OPTION);
-        if(ans == JOptionPane.YES_OPTION){
-            JOptionPane.showMessageDialog(null,"รักนะ");
-        }else{
-            JOptionPane.showMessageDialog(null,"จำไว้เลย");
-        }
+        JFrame frame = new JFrame();
+        Login loginPanel = new Login();
+        frame.setContentPane(loginPanel.getMainPanel());
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
